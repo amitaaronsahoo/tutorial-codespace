0f52fdcb9bea25cefcb33af438130cc673b82dff
amitaaronsahoo <amitaaronsahoo@gmail.com>
2026-06-25 16:51:28 -0400

    Add initial project files and configurations

---
+++ tutorials/StringLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace UtilityLibraries;
+
+public static class StringLibrary
+{
+    // Make sure "(string str)" or "(String? str)" is right here!
+    public static bool StartsWithUpper(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str)) // Line 7?
+            return false;
+
+        char ch = str[0];                   // Line 10?
+        return char.IsUpper(ch);
+    }
+}
